1e6251f24039c90c0d31db62dcdfc4c79816dad6
MINO\User <busster@naver.com>
2026-05-13 18:48:05 +0900

    jsp를 위한 라이브러리 세팅 완료

---
+++ formLogin/src/main/webapp/WEB-INF/views/test.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!doctype html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>Test</title>
+</head>
+<body>
+<h1>test.jsp</h1>
+</body>
+</html>
--- formLogin/src/test/java/com/formLogin/FormLoginApplicationTests.java
+++ formLogin/src/test/java/com/formLogin/FormLoginApplicationTests.java
@@ -1,13 +1,33 @@
 package com.formLogin;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class FormLoginApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void rootShowsTestJsp() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("test"))
+                .andExpect(forwardedUrl("/WEB-INF/views/test.jsp"));
     }
 
 }
