1ea041ad90e11181b836e0b057b29f9d49186edd
MINO\User <busster@naver.com>
2026-05-13 20:49:57 +0900

    로그인 전에는 익명유저가 들어가있고, 로그인 후에는 user 가 들어감 원래는 세션이 로그인 사용자를 저장해야하지만, 시큐리티가 세션을 관리하기 때문에 할 필요가 없다

--- formLogin/src/main/java/com/formLogin/controller/TestController.java
+++ formLogin/src/main/java/com/formLogin/controller/TestController.java
@@ -1,18 +1,26 @@
 package com.formLogin.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Slf4j
 public class TestController {
 
     @GetMapping("/login")
     public String test() {
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        log.info("test에서의 name:{}",name);
         return "login";
     }
 
     @GetMapping("/main")
-    public String main(){
+    public String mainPage(){
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        log.info("main에서의 name:{}",name);
+
 
         return "main";
     }
