8b0441197775ce7977e45707b6a90f26619e0acc
MINO\User <busster@naver.com>
2026-05-19 17:18:18 +0900

    authentication을 받아 토큰을 만드는 provider 생성

---
+++ jwt/src/main/java/com/jwt/security/jwt/JwtTokenProvider.java
@@ -0,0 +1,57 @@
+package com.jwt.security.jwt;
+
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+
+@Component
+public class JwtTokenProvider {
+
+
+    private final SecretKey key;
+    private final Long expiration;
+
+
+    public JwtTokenProvider(
+            @Value("${jwt.secret}") String secretKey
+            , @Value("${jwt.expiration}") Long expiration) {
+        this.key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
+        this.expiration = expiration;
+
+    }
+
+    public String createToken(Authentication authentication){
+        String id = authentication.getName();
+
+        String roles = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .filter(role->role.startsWith("ROLES"))
+                .collect(Collectors.joining(","));
+
+
+        Date now = new Date();
+        Date expiry = new Date(now.getTime()+expiration);
+
+        return Jwts.builder()
+                .subject(id)
+                .claim("roles",roles)
+                .issuedAt(now)
+                .expiration(expiry)
+                .signWith(key)
+                .compact();
+    }
+
+
+
+}
