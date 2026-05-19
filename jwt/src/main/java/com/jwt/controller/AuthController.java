d09d368dd3948eb793313af1dc6c74a869561415
MINO\User <busster@naver.com>
2026-05-19 17:20:55 +0900

    로그인 로직 뼈대 완료

--- jwt/src/main/java/com/jwt/controller/AuthController.java
+++ jwt/src/main/java/com/jwt/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.jwt.controller;
 
 
 import com.jwt.dto.LoginDto;
+import com.jwt.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @PostMapping("/api/login")
     public ResponseEntity<?> login(@RequestBody LoginDto dto) {
@@ -25,8 +27,10 @@ public class AuthController {
         //인증 성공 시 authentication으로 돌려받고 이걸로 토큰을 만드는 것
         //provider가 이 토큰에서 id를 뽑아 userDetailsService를 호출
         Authentication authentication = authenticationManager.authenticate(token);
+        String jwt = jwtTokenProvider.createToken(authentication);
 
 
-        return ResponseEntity.ok("로그인 완료");
+
+        return ResponseEntity.ok(jwt);
     }
 }
