503836cc76e9fe7c6f7a37d01fe496df802b9f61
MINO\User <busster@naver.com>
2026-05-19 13:44:52 +0900

    security  체인을 서블릿 체인에 등록

---
+++ formLogin2/src/main/java/com/formLogin2/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.formLogin2.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http
+                .formLogin(fl -> fl
+                        .loginPage("/login")
+                        .loginProcessingUrl("/loginProc")
+                        .defaultSuccessUrl("/api/main")
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/form")
+                        .authenticated()
+                ).csrf(csrf -> csrf.disable())
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .build();
+    }
+
+
+}
