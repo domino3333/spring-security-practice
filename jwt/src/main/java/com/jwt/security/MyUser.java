26445b9f2964d97aa051fc7d1b9da8c2c462af8e
MINO\User <busster@naver.com>
2026-05-19 17:05:37 +0900

    UserDetails 관련 파일 추가

---
+++ jwt/src/main/java/com/jwt/security/MyUser.java
@@ -0,0 +1,59 @@
+package com.jwt.security;
+
+import lombok.Builder;
+import lombok.Data;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+public class MyUser implements UserDetails {
+
+    private Long no;
+    private String id;
+    private String password;
+    private String name;
+    private List<GrantedAuthority> authorities;
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return id;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
