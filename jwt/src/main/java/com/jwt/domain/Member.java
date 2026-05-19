f976888b12241c770efff315a9c002ef02e95166
MINO\User <busster@naver.com>
2026-05-19 16:58:16 +0900

    도메인 추가

---
+++ jwt/src/main/java/com/jwt/domain/Member.java
@@ -0,0 +1,14 @@
+package com.jwt.domain;
+
+import lombok.Data;
+
+@Data
+public class Member {
+
+    private Long no;
+    private String id;
+    private String password;
+    private String name;
+
+
+}
---
+++ jwt/src/main/java/com/jwt/domain/MemberRole.java
@@ -0,0 +1,11 @@
+package com.jwt.domain;
+
+import lombok.Data;
+
+@Data
+public class MemberRole {
+
+    private Long no;
+    private String roleName;
+
+}
