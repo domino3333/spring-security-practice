23c4567e92ffcb62eedf7a9d1403a046ec577dd0
MINO\User <busster@naver.com>
2026-05-19 23:25:42 +0900

    Bearer 토큰 확인할 때 공백 포함해서 고려 7은 공백을 포함한 길이에 맞춘거니까

--- jwt/src/main/java/com/jwt/security/jwt/JwtAuthenticationFilter.java
+++ jwt/src/main/java/com/jwt/security/jwt/JwtAuthenticationFilter.java
@@ -31,7 +31,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             FilterChain filterChain) throws ServletException, IOException {
 
         String header = request.getHeader("Authorization");
-        if(header != null && header.startsWith("Bearer")){
+        if(header != null && header.startsWith("Bearer ")){
             String token = header.substring(7);
 
             if(jwtTokenProvider.validateToken(token)){
